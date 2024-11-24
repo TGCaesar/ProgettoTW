74fb4aee42403ae4c92e65c5f466acea11850306
TGCaesar <alessandro.decesare91@gmail.com>
2024-11-24 20:08:55 +0100

    Add files via upload

---
+++ Login/Log-in.js
@@ -0,0 +1,42 @@
+
+const Email = document.getElementById("input-email");
+const Password = document.getElementById("input-psw");
+const Button = document.getElementById("btn")
+let ErrorMsgs = {
+    NoEmail : document.createTextNode ("Inserire Email"),
+    NoPassword : document.createTextNode ("Inserire Password"),
+    ShortPassword : document.createTextNode ("La password deve avere almeno 8 caratteri"),
+    InvalidEmail : document.createTextNode ("Email non valida")
+}
+const ErrorP = document.getElementById ("ErrorMsg")
+
+function login () {
+    event.preventDefault ()
+    let user = {
+    username : Email.value,
+    password : Password.value,
+    };
+    if (user.username) {
+        if ( /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(user.username)) {
+        if (user.password) {
+            if (user.password.length>=8){
+            
+            console.log(user.username+" "+user.password);
+        } else {
+            if (ErrorP.hasChildNodes()) {ErrorP.removeChild(ErrorP.firstChild)}
+                ErrorP.appendChild(ErrorMsgs.ShortPassword)
+        }
+        
+        } else {
+            if (ErrorP.hasChildNodes()) {ErrorP.removeChild(ErrorP.firstChild)}
+                ErrorP.appendChild(ErrorMsgs.NoPassword)
+        }
+    } else {
+                if (ErrorP.hasChildNodes()) {ErrorP.removeChild(ErrorP.firstChild)}
+                ErrorP.appendChild(ErrorMsgs.InvalidEmail)
+           }
+    } else {
+        if (ErrorP.hasChildNodes()) {ErrorP.removeChild(ErrorP.firstChild)}
+        ErrorP.appendChild(ErrorMsgs.NoEmail)
+    }
+} 
